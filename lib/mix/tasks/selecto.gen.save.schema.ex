a7a9fcd320686a8cd7569fbe26fc10e49543cbf3
Chris Rohlfs <seeken@gmail.com>
2022-12-29 21:43:17 -0700

    stub acept opts

--- lib/mix/tasks/selecto.gen.save.schema.ex
+++ lib/mix/tasks/selecto.gen.save.schema.ex
@@ -2,9 +2,14 @@ defmodule Mix.Tasks.Selecto.Gen.Save.Schema do
   @moduledoc "The hello mix task: `mix help hello`"
   use Mix.Task
 
-  @shortdoc "Simply calls the Hello.say/0 function."
-  def run(_) do
+  @shortdoc "Generate Schema to save Selecto Views."
+  def run(args) do
     # calling our Hello.say() function from earlier
     IO.puts("HERE")
+
+    {_parsed, rest} = OptionParser.parse!(args)
+
+    IO.inspect( rest )
+
   end
 end
