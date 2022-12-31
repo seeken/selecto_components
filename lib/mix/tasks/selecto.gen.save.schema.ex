51c2b4759d192209d8320157e27570c90802667e
Chris Rohlfs <seeken@gmail.com>
2022-12-30 22:26:48 -0700

    stubs

--- lib/mix/tasks/selecto.gen.save.schema.ex
+++ lib/mix/tasks/selecto.gen.save.schema.ex
@@ -2,6 +2,14 @@ defmodule Mix.Tasks.Selecto.Gen.Save.Schema do
   @moduledoc "The hello mix task: `mix help hello`"
   use Mix.Task
 
+  @doc """
+  params:
+    tablename
+    modulename
+    contextfield1:type contextfield2:type ...
+
+  """
+
   @shortdoc "Generate Schema to save Selecto Views."
   def run(args) do
     # calling our Hello.say() function from earlier
@@ -10,6 +18,9 @@ defmodule Mix.Tasks.Selecto.Gen.Save.Schema do
     {_parsed, rest} = OptionParser.parse!(args)
 
     IO.inspect( rest )
+
+
+
 
   end
 end
