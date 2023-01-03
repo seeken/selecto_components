edea369ac64ecc11c7b4ee58ef2915f884a677c0
Chris Rohlfs <seeken@gmail.com>
2023-01-03 14:31:33 -0700

    more stubbing

--- lib/mix/tasks/selecto.gen.save.schema.ex
+++ lib/mix/tasks/selecto.gen.save.schema.ex
@@ -10,17 +10,36 @@ defmodule Mix.Tasks.Selecto.Gen.Save.Schema do
 
   """
 
+  # mix selecto.gen.save.schema SavedView saved_view
+
+  # .repo
+  # .module
+  # .schema
+  # .talename
+
+  ### Generate schema file priv/templates/save.schema.ex
+  ### Generate context file priv/templates/save.context.ex
+
   @shortdoc "Generate Schema to save Selecto Views."
   def run(args) do
     # calling our Hello.say() function from earlier
-    IO.puts("HERE")
 
-    {_parsed, rest} = OptionParser.parse!(args)
-
-    IO.inspect( rest )
-
+    schema = build(args)
+    IO.inspect(schema)
 
 
 
   end
+
+  defp build(args) do
+    {_parsed, [modname, plural_mod, tabname, repo] = _rest} = OptionParser.parse!(args, [])
+    %{
+        modname: modname,
+        plural_mod: plural_mod,
+        tablename: tabname,
+        repo: repo
+    }
+  end
+
+
 end
